53c149cd49051fcd2746d1759e7de750a1247916
alperscmz <alpersacmaozu@gmail.com>
2022-01-22 16:08:48 +0300

    tip_donusumleri
    vsc c# ile tip donusumleri

---
+++ tip_donusumleri/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+namespace tip_donusumleri
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+
+//Implicit Conversion(bilinçsiz dönüşüm)
+byte a=5;
+sbyte b=3;
+short c=3;
+
+int d=a+b+c;
+Console.WriteLine("d: " +d);
+
+ long h=d;
+ Console.WriteLine("h: " +h);
+
+ float i=h;
+ Console.WriteLine("i: " +i);
+
+ string e="alper";
+ char f='k';
+ object g=e+f+d;
+ Console.WriteLine("g:" +g);
+
+//Explicit Conversion(bilinçli dönüşüm)
+int x=4;
+byte y=(byte)x;
+Console.WriteLine("y:" +y);
+int z=100;
+byte t=(byte)z;
+Console.WriteLine("t: "+t);
+float w=10.3f;
+byte v=(byte)w;
+Console.WriteLine("v: "+v);
+
+//ToString 
+int xx=6;
+string yy=xx.ToString();
+Console.WriteLine("yy: "+yy);
+
+string zz=12.5f.ToString();
+Console.WriteLine("zz: "+zz);
+
+//System.Convert
+
+string s1="10",s2="20";
+int sayi1,sayi2;
+int Toplam;
+sayi1=Convert.ToInt32(s1);
+sayi2=Convert.ToInt32(s2);
+Toplam=sayi1+sayi2;
+Console.WriteLine("Toplam:" +Toplam);
+
+//Parse methodu
+ParseMethod();
+
+
+
+        }
+        public static void ParseMethod()
+        {
+string metin1="10";
+string metin2="10.25";
+int rakam1;
+double double1;
+    rakam1=Int32.Parse(metin1);
+    double1=Double.Parse(metin2);
+    Console.WriteLine("rakam1: " +rakam1);
+    Console.WriteLine("double1: " +double1);
+        }
+    }
+    
+}
+
